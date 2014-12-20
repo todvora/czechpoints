eec768d154f98e1fa534093e63658fb54a0051d1
Tomas Dvorak <todvora@gmail.com>
2014-12-20 06:52:53 +0000

    Creating site for 1.0-SNAPSHOT

---
+++ js/main.js
@@ -0,0 +1,164 @@
+var map = L.map('map').setView([49.8, 15], 8);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    maxZoom: 18
+}).addTo(map);
+
+var renderOpeningTimes = function (openingTimes) {
+    var names = {
+        'monday': 'Po',
+        'tuesday': 'Út',
+        'wednesday': 'St',
+        'thursday': 'Čt',
+        'friday': 'Pá',
+        'saturday': 'So',
+        'sunday': 'Ne'
+    };
+    var result = '';
+    for (var key in names) {
+        var czName = names[key];
+        var times = openingTimes[key];
+        if (times == null) {
+            times = "-";
+        }
+        result = result + czName + ": " + times + "<br>";
+    }
+    return result;
+};
+
+var renderLegend = function (markers, cluster, knownTypes) {
+
+    var addType = function (div, type) {
+
+        var typeLine = document.createElement('div');
+        var checkbox = $(document.createElement('input')).attr({
+            type: 'checkbox',
+            checked: 'checked'
+        });
+
+        $(checkbox).change(function () {
+            var checked = $(this).is(':checked');
+            markers.forEach(function (marker) {
+                if (marker.rawData.type == type) {
+                    if (checked) {
+                        cluster.addLayer(marker);
+                    } else {
+                        cluster.removeLayer(marker);
+                    }
+                }
+            });
+        });
+
+        $(typeLine).append(checkbox);
+        $(typeLine).append("&nbsp;" + type);
+        $(div).append(typeLine);
+
+//        this._div.innerHTML = this._div.innerHTML + "<input type='checkbox' checked='checked'/>" + key + "<br>"
+    };
+
+    var info = L.control({position: 'topright'});
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this._div.innerHTML = '<h4>Typy Czechpointů:</h4>';
+        for (var key in knownTypes) {
+            addType(this._div, key);
+        }
+        return this._div;
+    };
+    info.addTo(map);
+
+};
+
+var renderContacts = function (contacts) {
+    var renderLink = function(link, protocol) {
+       var href = link;
+       if(protocol != null) {
+          href = protocol + ":" + link;
+       }
+       var label = link.replace("http://", "").replace("https://", "");
+       if(label.length > 30) {
+            label = label.substring(0,30) + "...";
+       }
+       var link = $(document.createElement('a')).attr('href', href).text(label);
+       return link;
+    }
+
+    var table = $(document.createElement('table'));
+    contacts.forEach(function(contact){
+        if (contact.href != null) {
+            table.append(
+               $(document.createElement('tr')).append([
+                    $(document.createElement('td')).append(contact.label + ": "),
+                    $(document.createElement('td')).append(renderLink(contact.href, contact.protocol))
+                ])
+            );
+        }
+    });
+    return table;
+};
+
+
+var constructPopupContent = function (item) {
+
+    var content = $(document.createElement('div'));
+
+    if (item.name != null) {
+        content.append("<h2>" + item.name + " <small> - " + item.type + "</small></h2>");
+    } else {
+        content.append("<h2>" + item.type + "</h2>");
+    }
+
+    var street = item.street;
+
+    if (street.match(/^\d+$/)) {
+        street = item.city + " " + item.street;
+    }
+
+    content.append("<p>" + street + "<br>" + item.zip + " " + item.city + "</p>");
+
+    var contacts = [];
+    contacts.push({'label': 'Web', 'href': item.www});
+    contacts.push({'label':'E-mail', 'href':item.email, 'protocol':'mailto'});
+    contacts.push({'label':'Telefon', 'href': item.phone, 'protocol':'tel'});
+
+
+    if (item.type == 'Česká pošta') {
+        var url = "https://www.postaonline.cz/vyhledani-psc?p_p_id=psc_WAR_pcpvpp&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_psc_WAR_pcpvpp_struts.portlet.action=%2Fview%2FdetailPost&_psc_WAR_pcpvpp_struts.portlet.mode=view&_psc_WAR_pcpvpp_zipCode=" + item.zip + "&_psc_WAR_pcpvpp_form.zip=&_psc_WAR_pcpvpp_form.city="
+        contacts.push({'label': 'Web', 'href': url});
+    }
+
+    content.append(renderContacts(contacts));
+
+    if (item.openingTimes != null) {
+        content.append("<h4>Otevírací doba:</h4> " + renderOpeningTimes(item.openingTimes) + "</p>");
+    }
+
+    content.append(" <p>Geolokační data: " + item.location.licence + "</p>");
+    return content.html();
+};
+
+
+$.get("czechpoints.json", function (data) {
+    var knownTypes = {};
+    var cluster = new L.MarkerClusterGroup();
+    var allMarkers = [];
+    data.forEach(function (item) {
+        knownTypes[item.type] = true;
+        if (item.location !== null) {
+            var marker = L.marker([item.location.lat, item.location.lon]);
+            marker.rawData = item;
+            marker.on('click', function (e) {
+                this.unbindPopup();
+                marker.bindPopup(constructPopupContent(item));
+                this.openPopup();
+            });
+            allMarkers.push(marker);
+        } else {
+            //console.log("Czechpoint bez adresy:" + JSON.stringify(item));
+        }
+    });
+
+    cluster.addLayers(allMarkers);
+    map.addLayer(cluster);
+    renderLegend(allMarkers, cluster, knownTypes);
+});
