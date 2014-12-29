5cac41afc7418a0419d1d4d692a09c9c59fef824
Tomas Dvorak <todvora@gmail.com>
2014-12-29 20:45:03 +0100

    suggestbox keyboard event fix

--- src/site/resources/js/main.js
+++ src/site/resources/js/main.js
@@ -82,7 +82,7 @@ var renderSearchBox = function (markers, cluster) {
         }
     });
 
-    suggestBox.keypress(function() {
+    suggestBox.keypress(function(event) {
         if (event.which == 13) {
             event.preventDefault();
             onSuggestSelected();
